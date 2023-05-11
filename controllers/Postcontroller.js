e25fc74a65b698b342b897d9eee23a3f946a0609
Fathahudheen <vfathahu123@gmail.com>
2023-05-12 01:03:11 +0530

    initited travelR backend work

--- controllers/Postcontroller.js
+++ controllers/Postcontroller.js
@@ -1,24 +1,75 @@
-const Post = require('../models/Postmodel'); 
-const createPost = async(req,res)=>{
- 
-try{
+const Post = require("../models/Postmodel");
+const createPost = async (req, res) => {
+  try {
+    const post = new Post({
+      title: req.body.title,
+      date: req.body.date,
+      image: req.file.filename,
+    });
 
-const post  = new Post({
-    title:req.body.title,
-    date:req.body.date,
-    image:req.file.filename
-});
+    const postData = await post.save();
+    res.status(200).send({ success: true, msg: "Posted data", data: postData });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error.message });
+  }
+};
 
-const postData = await post.save(); 
-res.status(200).send({success:true,msg:'Posted data',data:postData});
+// To view Posts
 
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).send({ success: true, msg: "Posted data", data: posts });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error.message });
+  }
+};
 
-} catch(error){
-    res.status(400).send({success:false,msg:error.message});
-}
+// To Delete Posts
+const deletePosts = async (req, res) => {
+  try {
+    const id = req.params.id;
+    res.status(200).send({ success: true, msg: "Post Deleted Successfully" });
 
-}
+    await Post.deleteOne({ _id: id });
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error.message });
+  }
+};
+
+//Update posts
+const updatePost = async (req, res) => {
+  try {
+    if (req.file !== undefined) {
+      var id = req.body.id;
+      var title = req.body.title;
+      var date = req.body.date;
+      var filename = req.file.filename;
+
+      await Post.findByIdAndUpdate(
+        { _id: id },
+        { $set: { title: title, date: date, image: filename } }
+      );
+      res.status(200).send({ success: true, msg: "Post updated Successfully" });
+    } else {
+      var id = req.body.id;
+      var title = req.body.title;
+      var date = req.body.date;
+
+      await Post.findByIdAndUpdate(
+        { _id: id },
+        { $set: { title: title, date: date} }
+      );
+      res.status(200).send({ success: true, msg: "Post updated Successfully" });
+    }
+  } catch (error) {
+    res.status(400).send({ success: false, msg: error.message });
+  }
+};
 
 module.exports = {
-    createPost
-}
+  createPost,
+  getPosts,
+  deletePosts,
+  updatePost,
+};
--- routes/Postroute.js
+++ routes/Postroute.js
@@ -1,38 +1,55 @@
-const express = require('express');
+const express = require("express");
 const postRoute = express();
 
-const bodyPasrser = require('body-parser');
+const bodyPasrser = require("body-parser");
 postRoute.use(bodyPasrser.json());
-postRoute.use(bodyPasrser.urlencoded({extended:true}));
+postRoute.use(bodyPasrser.urlencoded({ extended: true }));
 
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
-
-postRoute.use(express.static('public'));
+postRoute.use(express.static("public"));
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb) =>{
-        cb(null,path.join(__dirname,'../public/postImages'),(error,success) => {
-            if(error){
-                console.log(error);
-            }
-        });
-    },
-    filename:(req,file,cb) =>{
-        const name = Date.now()+'-'+file.originalname; 
-            cb(null,name,(error,success) =>{
-                if(error){
-                    console.log(error);
-                }
-            });
-    }
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../public/postImages"), (error, success) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  },
+  filename: (req, file, cb) => {
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name, (error, success) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  },
 });
 
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 
-const postcontroller = require('../controllers/Postcontroller')
+const postcontroller = require("../controllers/Postcontroller");
 
-postRoute.post('/createpost',upload.single("image"),postcontroller.createPost);  
+postRoute.post(
+  "/createpost",
+  upload.single("image"),
+  postcontroller.createPost
+);
 
-module.exports =  postRoute;
+//  To view Posts
+
+postRoute.get("/getposts", postcontroller.getPosts);
+
+// To delete Posts
+postRoute.get("/deletepost/:id", postcontroller.deletePosts);
+
+//update posts
+postRoute.post(
+  "/updatepost",
+  upload.single("image"),
+  postcontroller.updatePost
+);
+
+module.exports = postRoute;
